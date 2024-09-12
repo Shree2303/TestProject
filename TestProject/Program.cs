6075d59e686b4227b3e83f520549ec6d40661d1c
Shree2303 <shreeprabhakaran2303@gmail.com>
2024-09-12 14:06:35 +0530

    Keeping one message

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,8 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, to GIT ! one small changes");
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, to GIT ! one small changes");
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, to GIT ! one small changes");
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, to GIT ! one small changes");
